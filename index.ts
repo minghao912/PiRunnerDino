10c8f8267329989acb82a337784aef94a086d1c9
minghao912 <minghaochen912@gmail.com>
2022-03-15 14:34:44 -0700

    Pi Runner Dino ver.

---
+++ index.ts
@@ -0,0 +1,100 @@
+// Elements
+const game = document.getElementById("game");
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const ground = document.getElementById("ground");
+const gameOverScreen = document.getElementById("game-over-screen");
+const digits = document.getElementById("digits");
+
+// Globals
+const PI1000 = "3141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067\
+                9821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819\
+                6442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127\
+                3724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609\
+                4330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491\
+                2983367336244065664308602139494639522473719070217986094370277053921717629317675238467481846766940513\
+                2000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923\
+                5420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185\
+                9502445945534690830264252230825334468503526193118817101000313783875288658753320838142061717766914730\
+                35982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989";
+let numDigits = 1;
+let gameIsOver = false;
+
+function checkNumber(num: number): boolean {
+    return (parseInt(PI1000[numDigits]) == num);
+}
+
+function dinoJump(keyPressed: string) {
+    if (gameIsOver)
+        return;
+
+    let numPressed = parseInt(keyPressed);
+    if (numPressed == NaN)
+        return;
+
+    if (checkNumber(numPressed)) {
+        // Cannot jump more than once at a time
+        if (dino.classList.contains("jump"))
+            return;
+
+        // Make dino jump
+        dino.classList.add("jump");
+
+        // Increment pointer to digit
+        numDigits++;
+
+        // Add digit to display
+        digits.innerText += numPressed;
+
+        // Remove the jump class after 0.3s
+        setTimeout(function() {
+            dino.classList.remove("jump");
+        }, 300);
+    } else {
+        game.classList.add("wrong-number");
+        setTimeout(function() {
+            dino.classList.remove("wrong-number");
+        }, 200);
+    }
+}
+
+function gameOver() {
+    // Hide play elements
+    dino.style.display = "none";
+    cactus.style.display = "none";
+    ground.style.display = "none";
+
+    // Show game over
+    gameOverScreen.style.display = "inline";
+    document.getElementById("score").innerText = (numDigits - 1).toLocaleString();
+
+    gameIsOver = true;
+
+    // Refresh to play again
+    document.addEventListener("keydown", (event) => {
+        if (event.key == "Enter")
+            location.reload();
+    });
+}
+
+// Check if dino is still alive on set interval
+let isAlive = setInterval(function() {
+    // console.log("Checking dino alive");
+    
+    // Check if dino position overlaps with cactus
+    // Get positions
+    let dinoTop: number = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft: number = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    // Collision detection
+    if (cactusLeft < 40 && cactusLeft > 0) {
+        if (dinoTop >= 140) {
+            // Collision
+            gameOver();
+        }
+    }
+}, 10);
+
+document.addEventListener("keydown", (event) => {
+    dinoJump(event.key);
+});
